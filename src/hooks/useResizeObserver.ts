1c00818ad89534410e3440d1d622549193389a34
Gianmarco Ricciarelli <gianmarcoricciarelli@gmail.com>
2025-03-17 15:03:21 +0100

    feat: Added check for tablet to useResizeObserver

--- src/hooks/useResizeObserver.ts
+++ src/hooks/useResizeObserver.ts
@@ -1,16 +1,23 @@
 import { useEffect, useState } from 'react'
 
 const MOBILE_THRESHOLD = 375
+const TABLET_THRESHOLD = 768
 
 export default function useResizeObserver() {
     const [isMobile, setIsMobile] = useState(false)
+    const [isTablet, setIsTablet] = useState(false)
 
     useEffect(() => {
         const observer = new ResizeObserver((entry) => {
             if (entry[0].contentRect.width <= MOBILE_THRESHOLD) {
                 setIsMobile(true)
+                setIsTablet(false)
+            } else if (entry[0].contentRect.width <= TABLET_THRESHOLD) {
+                setIsMobile(false)
+                setIsTablet(true)
             } else {
                 setIsMobile(false)
+                setIsTablet(false)
             }
         })
         observer.observe(document.body)
@@ -21,6 +28,7 @@ export default function useResizeObserver() {
     }, [])
 
     return {
-        isMobile
+        isMobile,
+        isTablet
     }
 }
